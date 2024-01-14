2452a49cf3e7729d10da812c66d6d8fb27facbac
AnyutaAntonovich <antonovich_am@bk.ru>
2024-01-14 20:30:12 +0600

    Add task5

--- hometaskapp/admin.py
+++ hometaskapp/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Service, Order, User
 from django.db.models import F
 
-
+#
 @admin.action(description='Count objects')
 def count_objects(modeladmin, request, queryset):
     queryset.update(count = 10)
@@ -16,12 +16,18 @@ def up_price(modeladmin, request, queryset):
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('name', 'email')
+    list_filter = ('name',)
+    search_fields = ('name', 'email', 'telephone')
+    fieldsets = (('User', {'fields': ('name', 'email', 'telephone', 'adress')}),)
+
+
+class UserService(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price')
     list_filter = ('price',)
-    search_fields = ('name', 'email')
-    fieldsets = (('User', {'fields': ('name', 'email', 'telephone', 'adress')}),
-                 ('Order', {'fields': ('customer', 'products', 'date_ordered', 'total_price')}),
-                 ('Service', {'fields': ('name', 'price', 'description', 'count', 'image')}))
+    search_fields = ('name', 'price')
+    fieldsets = (('Service', {'fields': ('name', 'price', 'description', 'count', 'image')}),)
     actions = (count_objects, up_price,)
 
 
-admin.site.register(User, Service, Order, UserAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Service, UserService)
